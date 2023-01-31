dee208da6327e260d188baf458b5fd17c1df01bb
KimGyeongyeon <kgy556@naver.com>
2023-01-31 17:23:20 +0900

    submit : 거짓말 ver 1
    - 진실을 아는 사람이 있는 자리에서는 진실을 말해야한다.
    - 따라서 진실을 아는 사람과 같은 파티장에 있는 사람도 진실을 알게된다.
    - 진실을 알게된 사람을 모두 찾은 다음 거짓말 할 수 있는 파티장을 찾아야한다.

---
+++ ct_23Feb/src/main/kotlin/week1/Lie.kt
@@ -0,0 +1,61 @@
+package week1
+
+import Solution
+
+class Lie: Solution {
+    override fun start() {
+        val res = parseInput{
+                truth, arr -> solution(truth, arr)
+        }
+        println(res)
+    }
+
+    fun parseInput(func: (HashSet<Int>, Array<HashSet<Int>>) -> Int): Int {
+        // 1st line
+        val numbers = readLine()?.split(" ") ?: return -1
+        if (numbers.size != 2) return -2
+
+        val totalMember = numbers[0]
+        val partyNumber = numbers[1].toIntOrNull() ?: return -3
+
+        // 2nd line
+        val truthInfo = readLine()?.split(" ") ?: return -4
+        val initialTruthMembers = hashSetOf<Int>()
+        for (i in 1 until truthInfo[0].toInt()) {
+            truthInfo[i].toIntOrNull()?.let {
+                initialTruthMembers.add(it)
+            }
+        }
+
+        // else
+        val parties = Array<HashSet<Int>>(partyNumber) { hashSetOf() }
+        for (i in 0 until partyNumber) {
+            val input = readLine()?.split(" ") ?: return -5
+
+            for (j in 1 until input.size) {
+                input[j].toIntOrNull()?.let {
+                    parties[i].add(it)
+                }
+            }
+        }
+
+        return func(initialTruthMembers, parties)
+    }
+
+    // set은 contains 연산을 constant time에 수행하기 때문에 유리하다.
+    fun solution(initialTruthMembers: HashSet<Int>, parties: Array<HashSet<Int>>): Int {
+        // 안들키고 과장할 수 있는 파티의 최댓값
+        val truthMembers: HashSet<Int> = HashSet(initialTruthMembers)
+
+        var lieCount = parties.size
+        for (i in parties.indices) {
+            val party = parties[i]
+            if (party.any { it -> truthMembers.contains(it) }) {
+                truthMembers.addAll(party)
+                lieCount--
+            }
+        }
+
+        return lieCount
+    }
+}
