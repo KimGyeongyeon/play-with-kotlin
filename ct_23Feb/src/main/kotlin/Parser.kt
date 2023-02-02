c7cb850aa6f313314f9cad83cc9c8bcfc8630a51
KimGyeongyeon <kgy556@naver.com>
2023-02-02 12:14:45 +0900

    feat : Parser의 함수를 최상위로 올림

--- ct_23Feb/src/main/kotlin/Parser.kt
+++ ct_23Feb/src/main/kotlin/Parser.kt
@@ -1,23 +1,21 @@
-object Parser {
-    fun getNbyMArray(n: Int, m: Int): Array<IntArray> {
-        val table = Array(n) { IntArray(m) }
-        for (i in 0 until n) {
-            val line = readln().split(" ")
-            for (j in 0 until m) {
-                table[i][j] = line[j].toInt()
-            }
+fun getNbyMArray(n: Int, m: Int): Array<IntArray> {
+    val table = Array(n) { IntArray(m) }
+    for (i in 0 until n) {
+        val line = readln().split(" ")
+        for (j in 0 until m) {
+            table[i][j] = line[j].toInt()
         }
-        return table
     }
+    return table
+}
 
-    fun getArrayWithLength(): IntArray {
-        val input = readLine()?.split(" ") ?: return intArrayOf()
-        val size = input[0].toIntOrNull() ?: return intArrayOf()
+fun getArrayWithLength(): IntArray {
+    val input = readLine()?.split(" ") ?: return intArrayOf()
+    val size = input[0].toIntOrNull() ?: return intArrayOf()
 
-        val arr = IntArray(size)
-        for (i in 1..size) {
-            arr[i - 1] = input[i].toInt()
-        }
-        return arr
+    val arr = IntArray(size)
+    for (i in 1..size) {
+        arr[i - 1] = input[i].toInt()
     }
+    return arr
 }
