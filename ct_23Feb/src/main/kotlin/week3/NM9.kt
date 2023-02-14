eb1196fc756dd52725be2756bd351ef24deaad66
KimGyeongyeon <kgy556@naver.com>
2023-02-14 21:15:17 +0900

    submit : N과 M (9)
    - 틀렸다고 뜬다.. 예외 케이스를 더 만들어봐야할 것 같다.

--- ct_23Feb/src/main/kotlin/week3/NM9.kt
+++ ct_23Feb/src/main/kotlin/week3/NM9.kt
@@ -1,54 +1,59 @@
 package week3
 
 import Solution
-import java.util.Stack
+import java.util.*
 
-class NM9(): Solution {
+class NM9() : Solution {
     private val SPACE = " "
     override fun start() {
-        parseInput().forEach{ println(it.joinToString(SPACE)) }
+        parseInput().forEach { println(it.joinToString(SPACE)) }
     }
 
     fun parseInput(): List<Array<Int>> {
-        val nm = readln().split(SPACE).map{it.toInt()}
-        val pool = readln().split(SPACE).map{it.toInt()}
-        return getPermutation(pool.sorted(), nm[1]).distinctBy{li -> li.joinToString("")}
+        val nm = readln().split(SPACE).map { it.toInt() }
+        val pool = readln().split(SPACE).map { it.toInt() }
+        return getPermutation(pool.sorted(), nm[1]).distinctBy { li -> li.joinToString("") }
     }
 
     fun getPermutation(pool: List<Int>, size: Int): List<Array<Int>> {
-        val result: MutableList<Array<Int>> = mutableListOf<Array<Int>>()
-        val startIndex = List<Int>(size){it}
-        var initialIndex = List<Int>(size){it}
-        val pick = Stack<Int>().apply{addAll(startIndex)}
-        val leftIndices = ArrayDeque<Int>().apply{addAll(size until pool.size)}
+        val result = mutableListOf<Array<Int>>()
+        val pick = Stack<Int>().apply { addAll(0 until size) }
+        val leftNums = PriorityQueue<Int>().apply { addAll(size until pool.size) }
 
-        while(true){
-            // pool에서 pick 인덱스 정보에 따라 수열 뽑기
-            result.add(Array(size){pool[pick[it]]})
+        while (true) {
+            result.add(Array(size) { pool[pick[it]] })
 
             // index 갱신
-            for(i in pick.indices.reversed()) {
-                val index = pick.pop().also{leftIndices.add(it)}
-                if(pick == initialIndex) {
-                    // 끝에 다다랐음
-                    pick.removeAt(i-1).let { leftIndices.add(it) }
-                    continue
+            for (i in pick.indices.reversed()) {
+                val max = leftNums.lastOrNull() ?: -1
+                if (pick[i] > max) {
+                    pick.pop().let { leftNums.add(it) }
                 } else {
                     break
                 }
             }
 
-            while(pick.size < size){
-                leftIndices.removeFirst().let{ pick.add(it) }
-            }
-
-            // 종료조건 검사
-            if(pick == initialIndex) {
+            if (pick.isEmpty()) {
+                // 종료조건
                 break
+            } else {
+                // 끝숫자의 다음으로 큰 수를 찾는다.
+                val currLast = pick.lastElement()
+                var nextLast = currLast
+                while (!leftNums.contains(nextLast)) {
+                    nextLast++
+                }
+                // 예전 값과 새 값을 바꾼다.
+                pick.pop().let { leftNums.add(it) }
+                leftNums.remove(nextLast)
+                pick.add(nextLast)
+
+                // 남은 자리는 작은 숫자로 채운다.
+                while (pick.size < size) {
+                    leftNums.poll().let { pick.add(it) }
+                }
             }
         }
-
         return result
     }
-
 }
