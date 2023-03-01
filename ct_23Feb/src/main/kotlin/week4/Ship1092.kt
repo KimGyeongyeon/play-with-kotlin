48a00ae7a0d8c34c5ab134827833bb29a6a7cec2
KimGyeongyeon <kgy556@naver.com>
2023-03-01 16:15:39 +0900

    save : ship ver 2

--- ct_23Feb/src/main/kotlin/week4/Ship1092.kt
+++ ct_23Feb/src/main/kotlin/week4/Ship1092.kt
@@ -2,62 +2,82 @@ package week4
 
 import Solution
 import kotlin.math.ceil
+import kotlin.math.min
 
 class Ship1092: Solution {
     override fun start() {
-        println("sdf")
-        getMinTime(
-            listOf(10, 5, 1),
-            listOf(2, 2, 2, 2, 2)
-        )
+        parseInput().let { println(it) }
     }
 
     val SPACE = " "
-    fun parseInput() {
+    fun parseInput(): Int {
         val n = readln().toInt() // n < 50. 크레인의 갯수
         val cranes = readln().split(SPACE).map { it.toInt() } // 크레인 무게 제한
         val m = readln().toInt() // m < 10^4. 박스 개수
         val boxes = readln().split(SPACE).map { it.toInt() } // 박스 무게
+        return getMinTime(
+            Port(
+                cranes, boxes
+            )
+        )
     }
 
-    fun getMinTime(cranes: List<Int>, boxes: List<Int>): Int {
-        // 모든 박스를 배로 옮기는데에 드는 시간의 최솟값
-        // 크레인은 1분에 박스 하나를 실을 수 있다.
-        // 크레인은 동시에 움직인다.
-        // 그럼 최대한 크레인을 많이 활용할 수 있게 해야겠네
-        // 박스 / 크레인 만큼씩 할당되는게 베스트!
-        val avg = ceil(boxes.size.toDouble() / cranes.size).toInt()
-        val cranesDes = cranes.sortedDescending()
-        val count = IntArray(cranes.size)
-        val boxDes = boxes.sortedDescending()
+    fun getMinTime(port: Port): Int {
 
-        // 일단 각 크레인마다 avg만큼 박스를 할당한다.
-        var i = 0
-        for (c in cranesDes.indices) {
-            if (cranesDes[c] >= boxDes[i] && count[c] < avg) {
-                count[c]++
-                i++
+        port.assignBox()
+
+        return port.craneCount.maxOf { it }
+    }
+}
+
+class Port(
+    cranes: List<Int>,
+    boxes: List<Int>
+) {
+    val craneDes = cranes.sortedDescending()
+    val boxDes = boxes.sortedDescending()
+    val craneCount = IntArray(cranes.size)
+    val avg = ceil(boxes.size.toDouble() / cranes.size).toInt()
+
+
+    fun assignBox() {
+        var cn = 0
+        for(box in boxDes) {
+            if (cn > craneDes.size) {
+                return // index 오류
             }
-        }
+            if (craneDes[cn] < box) {
+                return // 일어나면 안되는 상황
+            }
 
-        // 남은 박스를 처리한다.
-        var crane = 0
-        while (i < boxDes.size) {
-            if (cranesDes[crane] >= boxDes[i]) {
-                count[crane]++
-                i++
-                crane++
+            if (craneCount[cn] >= avg) {
+                // 평균보다 많이 수행하는 상황이다.
+                // 뒷 크레인에게 미룰 수 있으면 미룬다.
+                if (cn < craneDes.size - 1 && craneDes[cn+1] >= box) {
+                    cn++
+                    craneCount[cn]++
+                } else {
+                    // 앞 크레인 중 가장 한가한 크레인에게 넘긴다.
+                    val lazyCrane = findLazyCraneIndex(cn)
+                    craneCount[lazyCrane]++
+                }
+
             } else {
-                // box가 무거워서 앞쪽 크레인에게 맡겨야한다.
-                crane--
-            }
-
-            // 인덱스 조정
-            if (crane >= cranesDes.size) {
-                crane = 0
+                // box를 할당한다.
+                craneCount[cn]++
             }
         }
+    }
 
-        return count.maxOf { it }
+    fun findLazyCraneIndex(maxIndex: Int): Int {
+        var index = 0
+        var busy = Int.MAX_VALUE
+        for (c in 0 .. min(maxIndex, craneDes.size)) {
+            if (busy >= craneCount[c]) {
+                busy = craneCount[c]
+                index = c
+            }
+        }
+        return index
     }
 }
