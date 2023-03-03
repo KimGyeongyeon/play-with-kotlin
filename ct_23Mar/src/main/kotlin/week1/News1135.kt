058031e04307765e6062fcb0d4d0eeae86c17684
KimGyeongyeon <kgy556@naver.com>
2023-03-03 17:47:16 +0900

    pass : News ver 2
    - 재귀로 품

--- ct_23Mar/src/main/kotlin/week1/News1135.kt
+++ ct_23Mar/src/main/kotlin/week1/News1135.kt
@@ -1,38 +1,57 @@
 package week1
 
 fun main() {
-    println("news")
-    parseInput2()
+    println(input())
 }
 
-fun parseInput2() {
+fun input(): Int {
     val n = readln().toInt()
     val parents = readln().split(" ").map { it.toInt() }
 
+    // 함수 호출
     val tree = makeTree(n, parents)
-}
-
-// depth가 깊은 서브트리부터 전화하는게 포인트!
-//
-fun treeHeight(n: Int, parents: List<Int>): Int {
-
-    val internalNodes = hashSetOf<Int>()
-    var max = 0
-
-    for (i in 0 until n) {
-        var count = 0
-        var curr = i
-
-        if (internalNodes.contains(curr)) continue
-
-        while (parents[curr] != -1) {
-            count++
-            internalNodes.add(curr)
-            curr = parents[curr]
-        }
-        if (max < count) max = count
+    val root = tree.find { it.isRoot() }
+    return if (root != null) {
+        bfs(root, tree)
+    } else {
+        0 // root does not exist
     }
-
-    return max
 }
 
+fun bfs(n: Node, tree: ArrayList<Node>): Int {
+    if (n.isLeaf(-1)) return 0
+
+    val subTreeTimes = arrayListOf<Int>()
+    for (c in n.children) {
+        subTreeTimes.add(bfs(tree[c], tree))
+    }
+    return subTreeTimes
+        .sortedDescending()
+        .mapIndexed { index, i -> index + 1 + i } // 가장 연락이 오래 걸리는 부서부터 전화를 건다.
+        .maxOf { it }
+}
+
+//data class Node(
+//    val num: Int,
+//    val parent: Int,
+//    val children: ArrayList<Int>
+//) {
+//    fun isLeaf(): Boolean = children.isEmpty()
+//    fun isRoot(): Boolean = (parent == -1)
+//}
+//
+//fun makeTree(n: Int, parents: List<Int>): ArrayList<Node> {
+//    val tree = ArrayList<Node>(n)
+//    for (num in 0 until n) {
+//        val parent = parents[num]
+//        tree.add(Node(num, parent, arrayListOf()))
+//    }
+//
+//    for (num in 0 until n) {
+//        val parent = parents[num]
+//        if (parent in 0 until n) {
+//            tree[parent].children.add(num)
+//        }
+//    }
+//    return tree
+//}
